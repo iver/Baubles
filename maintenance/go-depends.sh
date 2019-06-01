4ccaf9cf9486fbe78c1509e1b239ae082c7d37d3
Ivan Jaimes <ivan.jaimes@iver.mx>
2019-06-01 08:20:32 -0500

    Add golang dependencies

---
+++ maintenance/go-depends.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# --
+# Author
+# --
+# Iván Jaimes <ivan@iver.mx>
+#
+# Goal script:  Install pre-requisites for spacemacs go-autocomplete layer.
+set -o errexit
+set -o nounset
+
+go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v github.com/zmb3/gogetdoc
+go get -u -v github.com/cweill/gotests/...
+go get -u github.com/haya14busa/gopkgs/cmd/gopkgs
+go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/josharian/impl
+
